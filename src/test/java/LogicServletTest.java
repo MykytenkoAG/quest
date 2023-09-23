bad01d3b125e1db21a67ef1b5e666401b8a89648
mykyt <mykytenko.a.g@gmail.com>
2023-09-23 17:40:49 +0300

    v0.37

--- src/test/java/LogicServletTest.java
+++ src/test/java/LogicServletTest.java
@@ -11,27 +11,11 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class LogicServletTest {
-    private final static String path = "/settings.jsp";
+
     @Test
     public void testDoPost() throws Exception{
 
-        ServletConfig sc = mock( ServletConfig.class );
-        ServletContext ctx = mock( ServletContext.class );
-        when( sc.getServletContext()).thenReturn( ctx );
 
-        ResetGamesCountServlet clearGamesCountServlet = new ResetGamesCountServlet();
-        clearGamesCountServlet.init( sc );
-
-        HttpServletRequest request = mock(HttpServletRequest.class);
-        HttpServletResponse response = mock(HttpServletResponse.class);
-        RequestDispatcher dispatcher = mock(RequestDispatcher.class);
-        HttpSession httpSession = mock(HttpSession.class);
-
-        when(request.getSession()).thenReturn(httpSession);
-        when(ctx.getRequestDispatcher(path)).thenReturn(dispatcher);
-
-/*        clearGamesCountServlet.doPost(request, response);
-        verify(dispatcher).forward(request, response);*/
 
     }
 }
